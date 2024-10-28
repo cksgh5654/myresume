c72cecb21b6c9f08bd9c2bfc01f3a36e5a8b8ba1
박찬호 <qkrcksgh5654@gmail.com>
2024-10-28 16:01:19 +0900

    init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const handleClickMenu = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <>
+      <header className="app-header">
+        <button onClick={() => handleClickMenu("/")}>home</button>
+        <button onClick={() => handleClickMenu("/portfolio")}>portfolio</button>
+        <button onClick={() => handleClickMenu("/contact")}>contact</button>
+      </header>
+    </>
+  );
+};
+
+export default Header;
